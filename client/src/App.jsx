2634d1509102b0c5344a48095fc754bd2217fb60
Telu-Praveen <praveentelu2024@outlook.com>
2024-05-29 07:06:53 +0530

    login route updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route exact path="/login" element={<Login/>}/>
+      <Route exact path="/" element={<Login/>}/>
       <Route exact path="/register" element={<Register/>}/>
       <Route exact path="/dashboard" element={<Dashboard/>}/>
       
