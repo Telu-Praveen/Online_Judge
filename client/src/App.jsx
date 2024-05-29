3d6268a862f94f104f27036759ce8633f2207ee5
Telu-Praveen <praveentelu2024@outlook.com>
2024-05-29 12:38:03 +0530

    register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<Login/>}/>
-      <Route exact path="/register" element={<Register/>}/>
+      <Route exact path="/login" element={<Login/>}/>
+      <Route exact path="/" element={<Register/>}/>
       <Route exact path="/dashboard" element={<Dashboard/>}/>
       
       <Route exact path="/solve/:id" element={<Solve/>}/>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -149,7 +149,7 @@ export default function Register() {
   
             <p className="mt-10 text-center text-sm text-gray-500">
               Already a member?{' '}
-              <a href="/" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"  >
+              <a href="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"  >
                 Login
               </a>
             </p>
